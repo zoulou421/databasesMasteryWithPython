3a062661d34c20dfa4c8b6aa87911d2106a60342
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-13 16:12:15 +0200

    added: write in json file

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ file_path ="settings.json"
 with open(file_path, "r") as f:
     settings = json.load(f)
 
-print(settings)
+print(settings) #key:value
+settings["fontSize"]=30
+
+with open(file_path, "w") as f:
+      json.dump(settings,f, indent=4)
+
+print(settings.get("fontSize"))#value
