b3bfe52cf020ea6624a53499bb63fec4b17728d6
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-13 17:25:16 +0200

    added:table created #sqlite #python

--- use_sqlite.py
+++ use_sqlite.py
@@ -1,4 +1,15 @@
 import sqlite3
 
 conn=sqlite3.connect("database.db")#if a database not exists python will create it
+
+#Create a table :first retrieve cursor
+myCursor=conn.cursor() #cursor allow to excute sql request
+
+myCursor.execute("""
+CREATE TABLE IF NOT EXISTS employees(
+     first_name text,
+     name text
+)
+""")
+conn.commit()
 conn.close() #close connection
