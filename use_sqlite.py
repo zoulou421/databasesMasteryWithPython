fee2e4738a59f63e5548ef62a3e11fa6df4695bb
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-14 06:59:09 +0200

    added:retrieve data into database SQLITE

--- use_sqlite.py
+++ use_sqlite.py
@@ -13,5 +13,11 @@ CREATE TABLE IF NOT EXISTS employees(
 """)
 dic = {"a": "Bonevy", "b": "BEBY"}
 conn.execute("INSERT INTO employees VALUES(:a, :b)", dic)
+d = {"a": "Bonevy"}
+myCursor.execute("SELECT *FROM employees WHERE first_name=:a", d)
+myData=myCursor.fetchall() #represents a generator and can only be executed one time per request//
+#also see: fetchone and test it.
+
+print(myData)
 conn.commit()
 conn.close()  # close connection
