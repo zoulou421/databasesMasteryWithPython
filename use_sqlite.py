0e0f6b463d139bba4599ccd71498b8698d44ce3f
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-14 07:54:13 +0200

    added:DELETE data from table employees

--- use_sqlite.py
+++ use_sqlite.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("database_new.db")  # if a database not exists python will create it
+conn = sqlite3.connect("database.db")  # if a database not exists python will create it
 
 # Create a table :first retrieve cursor
 myCursor = conn.cursor()  # cursor allow to execute sql request
@@ -26,14 +26,18 @@ CREATE TABLE IF NOT EXISTS employees_update(
      salary int
 )
 """)
-
+#INSERT
 dic = {"a": "Bonevy", "b": "BEBY", "c": 5987}
 conn.execute("INSERT INTO employees_update VALUES(:a, :b,:c)", dic)
 
+#UPDATE
 dic_v2 = {"salary": 35000, "first_name": "Bonevy", "last_name": "BEBY"}
 myCursor.execute("""UPDATE employees_update SET salary=:salary
 WHERE first_name=:first_name AND last_name=:last_name
 """, dic_v2)
+
+#DELETE
+myCursor.execute("DELETE FROM employees WHERE first_name='Bonevy'")
 
 conn.commit()
 conn.close()  # close connection
