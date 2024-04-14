154b62744160b7c2ad091b60252a23eacd874e83
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-14 07:43:41 +0200

    added:Update data into SQLITE/install extension to access interface

--- use_sqlite.py
+++ use_sqlite.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")  # if a database not exists python will create it
+conn = sqlite3.connect("database_new.db")  # if a database not exists python will create it
 
 # Create a table :first retrieve cursor
-myCursor = conn.cursor()  # cursor allow to excute sql request
-
+myCursor = conn.cursor()  # cursor allow to execute sql request
+'''
 myCursor.execute("""
 CREATE TABLE IF NOT EXISTS employees(
      first_name text,
@@ -17,7 +17,23 @@ d = {"a": "Bonevy"}
 myCursor.execute("SELECT *FROM employees WHERE first_name=:a", d)
 myData=myCursor.fetchall() #represents a generator and can only be executed one time per request//
 #also see: fetchone and test it.
-
 print(myData)
+'''
+myCursor.execute("""
+CREATE TABLE IF NOT EXISTS employees_update(
+     first_name test,
+     last_name text,
+     salary int
+)
+""")
+
+dic = {"a": "Bonevy", "b": "BEBY", "c": 5987}
+conn.execute("INSERT INTO employees_update VALUES(:a, :b,:c)", dic)
+
+dic_v2 = {"salary": 35000, "first_name": "Bonevy", "last_name": "BEBY"}
+myCursor.execute("""UPDATE employees_update SET salary=:salary
+WHERE first_name=:first_name AND last_name=:last_name
+""", dic_v2)
+
 conn.commit()
 conn.close()  # close connection
