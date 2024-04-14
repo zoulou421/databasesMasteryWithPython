b73ed50122ca8d4aeb75256fa039fa550936ad9e
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-14 09:26:38 +0200

    added:Multiline insert TinyDB

--- app_tinydb.py
+++ app_tinydb.py
@@ -7,9 +7,14 @@ from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 
 # Disk storage option
-# db = TinyDB('data_tinydb.json')  # it will create database:data_tinydb.json
+# db = TinyDB('data_tinydb.json')  # it will create database:data_tinydb.json //this is a relative path
 # db=TinyDB(storage=MemoryStorage)  # allowing to use in memory method
 
 # let use on DISK:you can add indent parameter
 db = TinyDB('data_tinydb.json', indent=4)
-db.insert({"name": "Bonevy", "Score": 100})
+# db.insert({"name": "Bonevy", "Score": 100})
+# Multiline insert
+db.insert_multiple([
+    {"name": "Bonevy", "Score": 100},
+    {"name": "Laurore", "Score": 900}
+])
